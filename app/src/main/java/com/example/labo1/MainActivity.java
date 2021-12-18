4dc90ff22181e7afbc30c358cbd5f5d13d9c1f2e
1032189159 <1032189159@pfur.ru>
2021-12-18 05:29:19 +0300

    Обновил

--- app/src/main/java/com/example/labo1/MainActivity.java
+++ app/src/main/java/com/example/labo1/MainActivity.java
@@ -4,28 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Pair;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements MyFragmentI {
 
     // переключатель относительно требуемого условия задания (открыть активность или фрагмент)
-    public static boolean openFragment = true;
+    public static boolean openFragment = false;
     public static String text = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        replaceFragment(new MyFragment());
-    }
-
-    void replaceFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.container, fragment)
+                .replace(R.id.container, new MyFragment())
                 .addToBackStack(null).commit();
     }
 
@@ -40,6 +33,9 @@ public class MainActivity extends AppCompatActivity implements MyFragmentI {
         Bundle b = new Bundle();
         b.putString(text, str);
         fragment.setArguments(b);
-        replaceFragment(fragment);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .addToBackStack(null).commit();
     }
 }
